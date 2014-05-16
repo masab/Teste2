46550a3159bd0326043a877b5d5028c83681baf8
masab <mariana.masabe@live.com>
2014-05-16 16:19:04 -0300

    Teste

---
+++ calculadora/src/calculadora/Principal.java
@@ -0,0 +1,21 @@
+package calculadora;
+
+import javax.swing.JOptionPane;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		calculo calculo = new calculo();
+		
+		int numero1;
+		int numero2;
+		
+		String casa ( JOptionPane.showInputDialog("Mensagem")); 
+		 	
+		
+		
+		
+	}
+
+}
---
+++ calculadora/src/calculadora/calculo.java
@@ -0,0 +1,9 @@
+package calculadora;
+
+public class calculo {
+	 double numero1;
+	 double numero2;
+	 String operador;
+	 
+}
+	
